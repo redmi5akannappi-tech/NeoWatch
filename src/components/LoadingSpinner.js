04a0bbdbc367174aa5484129729a46d0833bee27
redmi-derosky <redmi5akannappi@gmail.com>
2025-11-25 17:37:37 +0000

    initial

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex justify-center items-center p-4">
+      <div className="animate-spin h-8 w-8 border-4 border-blue-500 border-t-transparent rounded-full" />
+    </div>
+  );
+}
