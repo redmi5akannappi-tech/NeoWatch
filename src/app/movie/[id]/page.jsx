3c45985b52684d96059b427a21640e15e82ba440
redmi-derosky <redmi5akannappi@gmail.com>
2025-11-25 18:30:12 +0000

    movieAgain

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import LoadingSpinner from "../../components/LoadingSpinner"; // adjust this path if needed
+import LoadingSpinner from "../../../components/LoadingSpinner";
 
 export default function MovieDetail({ params }) {
   const router = useRouter();
