2e7f9a9574464391f9843d413acace9ae0852d00
redmi-derosky <redmi5akannappi@gmail.com>
2025-11-25 22:05:48 +0530

    Update page.jsx

--- src/app/watch/[id]/page.jsx
+++ src/app/watch/[id]/page.jsx
@@ -3,12 +3,18 @@ import axios from "axios";
 
 export default async function WatchMovie({ params }) {
   const { id } = params;
-  const embedUrl = `https://vidsrc.icu/embed/movie/${id}`;
   const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
   const response = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
   );
+
   const movie = response.data;
+
+  // Use IMDb ID instead of TMDB ID
+  const imdbId = movie.imdb_id;
+  const embedUrl = `https://vidsrc.icu/embed/movie/${imdbId}`;
+
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-2xl font-bold mb-4">{movie.title}</h1>
@@ -22,5 +28,3 @@ export default async function WatchMovie({ params }) {
     </div>
   );
 }
-
-
