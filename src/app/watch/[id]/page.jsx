64f192aca7b75640a0ff66019b39510d94cdbcc7
redmi-derosky <redmi5akannappi@gmail.com>
2025-11-25 19:17:05 +0000

    watchId

--- src/app/watch/[id]/page.jsx
+++ src/app/watch/[id]/page.jsx
@@ -5,26 +5,27 @@ export default async function WatchMovie({ params }) {
   const { id } = params;
   const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`,
+    { next: { revalidate: 60 } }
   );
 
-  const movie = response.data;
+  const movie = await response.json();
 
-  // Use IMDb ID instead of TMDB ID
   const imdbId = movie.imdb_id;
   const embedUrl = `https://vidsrc.icu/embed/movie/${imdbId}`;
 
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-2xl font-bold mb-4">{movie.title}</h1>
+
       <div className="w-full max-w-4xl">
         <iframe
-    className="w-full h-[500px] border-none rounded-lg"
-    allowFullScreen
-    scrolling="no"
-    src={embedUrl}
-    />
+          className="w-full h-[500px] border-none rounded-lg"
+          allowFullScreen
+          scrolling="no"
+          src={embedUrl}
+        />
       </div>
     </div>
   );
