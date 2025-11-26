1e8c868136acaa7aa1877336d5c2e6889a1ed3ef
redmi-derosky <redmi5akannappi@gmail.com>
2025-11-26 06:15:52 +0000

    MetaDataNewFile

---
+++ src/app/page.metadata.js
@@ -0,0 +1,23 @@
+export default function metadata() {
+  return {
+    title: "NeoWatch - Watch Free Movies, Anime & TV Shows",
+    description:
+      "Watch anime series, movies and TV shows for free. NeoWatch offers popular movies, trending anime and latest episodes with fast streaming and no signup.",
+    keywords: [
+      "free anime",
+      "watch anime online",
+      "free movies",
+      "free tv series",
+      "anime streaming",
+      "movie streaming",
+      "watch for free",
+    ],
+    openGraph: {
+      title: "NeoWatch - Free Movies & Anime Streaming",
+      description:
+        "Stream movies, anime and TV series online for free. High-quality and fast streaming without signup.",
+      url: "https://neo-watch-chi.vercel.app",
+      siteName: "NeoWatch",
+    },
+  };
+}
